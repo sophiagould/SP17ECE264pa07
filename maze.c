3827f11fb5789b7f12c7ffd3530481ffb8f42cf3
Milind Kulkarni <milind@purdue.edu>
2017-02-16 17:57:30 -0500

    initial commit of assignment template

---
+++ maze.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "maze.h"
+
+#ifndef READMAZE_OFF
+Maze * readMaze(char * mazeFilename) {
+
+	//FILL IN
+
+}
+#endif //#ifndef READMAZE_OFF
+
+#ifndef FREEMAZE_OFF
+void freeMaze(Maze * m) {
+	//1. Free each of the "rows" of the maze
+	
+	//2. Free the "spine" of the array
+	
+}
+#endif //#ifndef FREEMAZE_OFF
---
+++ path.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "path.h"
+
+#ifndef READPATH_OFF
+char * readPath(char * pathFilename) {
+
+	//FILL IN
+
+}
+#endif //#ifndef READPATH_OFF
+
+#ifndef CHECKPATH_OFF
+bool checkPath(Maze * m, char * path) {
+
+	//FILL IN
+
+}
+#endif //#ifndef CHECKPATH_OFF
