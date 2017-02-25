902c14edb0907e1286309a42ec96b41e43818785
Sophia Alexandra Gould <gould7@ecegrid-thin1.ecn.purdue.edu>
2017-02-24 19:22:02 -0500

    first

--- maze.c
+++ maze.c
@@ -5,16 +5,63 @@
 #ifndef READMAZE_OFF
 Maze * readMaze(char * mazeFilename) {
 
-	//FILL IN
+	FILE* mafi = fopen(mazeFilename, "r");
+	int test;
+	Maze* maMaze = malloc(sizeof(*maMaze));
+	test = fscanf(mafi, "%d %d", &(maMaze -> height), &(maMaze -> width));
+	if(test != 2){
+		return NULL;
+	} 
+	
+    maMaze -> maze = malloc((maMaze -> height) * sizeof(MazeSquare*));
+	int i;
+	for(i = 0; i < (maMaze -> height); i++)
+	{
+		maMaze -> maze[i] = malloc((maMaze -> width) * sizeof(MazeSquare*));	}
 
+	char crct = fgetc(mafi);
+    int hor;
+	int ver;
+	for(hor = 0; hor < (maMaze -> height); hor++){
+		for(ver = 0; ver < (maMaze -> width); ver++){
+			if(!feof(mafi)){
+				crct = fgetc(mafi);
+				if(crct != '\n'){
+					if(crct == WALL){
+						maMaze -> maze[hor][ver].type = WALL;
+						maMaze -> maze[hor][ver].visited = false;
+					}else if(crct == SPACE){
+						maMaze -> maze[hor][ver].type = SPACE;	
+						maMaze -> maze[hor][ver].visited = false;
+					}else if(crct == START){
+						maMaze -> maze[hor][ver].type = START;
+						maMaze -> maze[hor][ver].visited = false;
+						maMaze -> start.xpos = hor;
+						maMaze -> start.ypos = ver;
+					}else{
+						maMaze -> maze[hor][ver].type = END;
+						maMaze -> maze[hor][ver].visited = false;
+						maMaze -> end.xpos = hor;
+						maMaze -> end.ypos = ver;
+					}
+				}
+			}
+		}
+	    crct = fgetc(mafi);
+	}
+	fclose(mafi);
+	return(maMaze);
 }
 #endif //#ifndef READMAZE_OFF
 
 #ifndef FREEMAZE_OFF
 void freeMaze(Maze * m) {
 	//1. Free each of the "rows" of the maze
-	
-	//2. Free the "spine" of the array
-	
+	int x;
+	for (x = 0; x < (m -> height); x++){
+		free(m -> maze[x]);
+	}
+	//2. Free the "spine" of the array	
+	free(m -> maze);
 }
 #endif //#ifndef FREEMAZE_OFF
--- path.c
+++ path.c
@@ -1,12 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "path.h"
+#include <string.h>
 
 #ifndef READPATH_OFF
 char * readPath(char * pathFilename) {
-
+	FILE* mafi = fopen(pathFilename, "r");
 	//FILL IN
+	char crct;
+	int x = 0;
+	while(!feof(mafi)){
+		crct = fgetc(mafi);
+		if(crct != '\337'){
+			x++;
+		}
+	}
+	fseek(mafi,0,SEEK_SET);
+	
+	char * path = malloc(1 + (sizeof(*path) * x));
+	int y = 0;
+	while(x>y){
+		fscanf(mafi, "%c", &path[y++]);
+	}
+	path[y] = '\0';
 
+	fclose(mafi);
+	return(path);
 }
 #endif //#ifndef READPATH_OFF
 
@@ -14,6 +33,83 @@ char * readPath(char * pathFilename) {
 bool checkPath(Maze * m, char * path) {
 
 	//FILL IN
-
+	MazePos pos = m -> start;
+	MazePos finish = m -> end;
+	int hor = pos.xpos;
+	int ver = pos.ypos;
+	int fhor = finish.xpos;
+	int fver = finish.ypos;
+	int x = 0;
+	int test = path[x];
+	while(test != '\0'){
+		if(path[x] == SOUTH){
+			hor++;
+			if(hor <= (m -> height -1) && hor >= 0 && ver <= (m -> width - 1) && ver >= 0){
+				if(m -> maze[hor][ver].type == WALL || m -> maze[hor][ver].visited ==true){
+					return false;
+				}
+				if(m -> maze[hor][ver].type == END){
+					return true;
+				}
+				m -> maze[hor][ver].visited = true;
+			}
+			else{
+				return false;
+			}
+		}
+		if(path[x] ==NORTH){
+			hor --;
+			if(hor <= (m -> height - 1) && hor >= 0 && ver <= (m -> width -1) && ver >=0){
+				if(m -> maze[hor][ver].type == WALL || m -> maze[hor][ver].visited ==true){
+					return false;
+				}
+				if(m -> maze[hor][ver].type == END){
+					return true;
+				}
+				m -> maze[hor][ver].visited = true;
+			}
+			else{
+				return false;
+			}
+		}
+		if(path[x] ==WEST){
+			ver--;
+			if (hor <= (m -> height - 1) && hor >= 0 && ver <= (m -> width - 1) && ver >= 0){
+				if(m -> maze[hor][ver].type ==WALL || m -> maze[hor][ver].visited == true){
+					return false;
+				}
+				if(m -> maze[hor][ver].type ==END){
+					return true;
+				}
+				m -> maze[hor][ver].visited = true;
+			}
+			else{
+				return false;
+			}
+		}
+		if (path[x] == EAST){
+			ver++;
+			if(hor <= (m -> height - 1) && (hor >= 0) && ver <= (m -> width - 1) && (ver >= 0)){
+				if(m -> maze[hor][ver].type == WALL || m -> maze[hor][ver].visited == true){
+					return false;
+				}
+				if(m -> maze[hor][ver].type == END){
+					return true;
+				}
+				m -> maze[hor][ver].visited = true;
+			}
+			else{
+				return;
+			}
+		}
+		x++;
+		test = path[x];
+	}
+	if(m -> maze[fhor][fver].visited == true){
+		return true;
+	}
+	else{
+		return false;
+	}
 }
 #endif //#ifndef CHECKPATH_OFF
